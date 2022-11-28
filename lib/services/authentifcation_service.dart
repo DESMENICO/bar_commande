0cdccab4c27842d14eaec4e414b6f78b92520c87
ThibautGlissoux <la208792@student.helha.be>
2022-11-28 15:08:50 +0100

    user firestore auth

--- lib/services/authentifcation_service.dart
+++ lib/services/authentifcation_service.dart
@@ -1,6 +1,7 @@
+import 'package:bar_commande/services/firestore_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:bar_commande/models/user.dart' as Models;
-import 'package:flutter/cupertino.dart';
+
 
 
 
@@ -9,8 +10,7 @@ class AuthentificationService{
 
   Models.User userFromFirebase(User? user){
 
-
-     return Models.User(user!.uid);
+     return Models.User.auth(user!.uid);
   }
 
   Future<Models.User> signinUser(String mail, String password) async {
@@ -18,6 +18,15 @@ class AuthentificationService{
     User? user = firebaseResult.user;
     return userFromFirebase(user);
   }
+
+  Future<Models.User> 
+  createUser(String mail, String password) async {
+    UserCredential firebaseResult = 
+    await _auth.createUserWithEmailAndPassword(email: mail, password: password);
+   User? user = firebaseResult.user;
+    return userFromFirebase(user);
+  } 
+
 
 }
 
